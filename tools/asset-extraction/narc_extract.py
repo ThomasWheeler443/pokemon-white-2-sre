79209e46edeebbb7ca48625d1c045007f79fde67
ThomasWheeler443 <ThomasWheeler443@gmail.com>
2026-03-28 13:55:28 -0400

    Update names

--- tools/asset-extraction/narc_extract.py
+++ tools/asset-extraction/narc_extract.py
@@ -1,10 +1,10 @@
-# narc-extract.py
+# narc_extract.py
 # thomwheeler04@vt.edu - thomaswheeler443@gmail.com
 #
 # Extract proprietary 'Nitro Archive (NARC)' files
 # Into the base contents
 
-from assets.narc import Narc
+from asset_tools.narc import Narc
 
 def main():
     pass
